480c86bebc81e7347f30e6297319fdbc22536061
aliSami22 <alisamielsayed22@gmail.com>
2025-11-07 13:45:23 +0200

    edit

--- javaScript/register.js
+++ javaScript/register.js
@@ -29,3 +29,27 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 var userData = JSON.parse(localStorage.getItem("userForm"));
+
+// login
+var loginEmail = userData.email;
+var loginPassword = userData.password;
+console.log(loginEmail, loginPassword);
+var loginForm = document.querySelector(".loginForm");
+var emailLogin = document.querySelector(".emailLogin");
+var passwordLogin = document.querySelector(".passwordLogin");
+var validationMassage = document.querySelector(".validationMassage");
+document.addEventListener("DOMContentLoaded", function () {
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (
+      loginEmail == emailLogin.value &&
+      loginPassword == passwordLogin.value
+    ) {
+      console.log("Login Success");
+      window.location.href = "home.html";
+    } else {
+      validationMassage.textContent = "Email or Password not valid";
+    }
+  });
+});
